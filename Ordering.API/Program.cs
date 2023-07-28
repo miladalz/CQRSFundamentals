808efed0e243e6629679326b1be5c7263324c208
miladalz <milad7alizadeh@gmail.com>
2023-07-28 16:32:49 +0330

    create order

---
+++ Ordering.API/Program.cs
@@ -0,0 +1,49 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.AggregatesModel.BuyerAggregate;
+using Ordering.Domain.AggregatesModel.OrderAggregate;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Repositories;
+using System.Reflection;
+using FluentValidation.AspNetCore;
+using Ordering.API.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+builder.Services.AddDbContext<OrderingContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionString"]);
+});
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IBuyerRepository, BuyerRepository>();
+builder.Services.AddTransient<OrderingContextSeed>();
+
+builder.Services.AddControllers()
+    .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssembly(typeof(Program).Assembly));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetService<OrderingContextSeed>().SeedAsync().Wait();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
